cac2388710d81382502161607f26c081af8cba19
martypdx <marty@martypdx.com>
2016-10-19 10:20:10 -0700

    quicky fizz buzz

---
+++ test/fizz-buzz.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const fizzBuzz = require('../lib/fizz-buzz');
+
+describe('fizz-buzz', function() {
+
+    it('counts to 16 correctly', function() {
+        assert.deepEqual(fizzBuzz(16), [
+            1, 
+            2, 
+            'fizz',     // 3
+            4, 
+            'buzz',     // 5
+            'fizz',     // 6
+            7, 
+            8, 
+            'fizz',     // 9
+            'buzz',     // 10
+            11,
+            'fizz',     // 12
+            13,
+            14,
+            'fizzbuzz', // 15
+            16
+        ]);
+    });
+
+});
